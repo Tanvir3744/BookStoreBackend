e4c66e1de08e66e62ad0ecf8805964af7e455a8b
Tanvir3744 <biplobmohammad1@gmail.com>
2024-08-01 03:03:09 +0600

    essential interfaces has done

---
+++ .history/src/interface/common_20240801030229.ts
@@ -0,0 +1,15 @@
+import { IGenericErrorMessage } from "./error"
+
+export type IGenericErrorResponse = {
+    statusCode: number, 
+    message: string, 
+    errorMessage: IGenericErrorMessage[],
+}
+
+export type IGenericResponse = {
+    meta: {
+        page: number, 
+        limit: number, 
+        total: number, 
+    }
+}
